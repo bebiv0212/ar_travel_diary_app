03b76675b924581e367936bc1cb7758eb87afc68
ji200 <ji200407@naver.com>
2025-09-12 23:56:29 +0900

    bottom sheet 제작

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:joljak/widgets/bottom_sheet.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
@@ -9,10 +10,22 @@ class MapScreen extends StatelessWidget {
       body: SafeArea(
           child: Column(
             children: [
-              Text('map screen')
+              Text('map screen'),
+              Expanded(child: Stack(children: [
+                Align(
+                  alignment: Alignment.bottomCenter,child:
+              DraggableScrollableSheet(
+                  initialChildSize: 0.5,//기본 크키
+                  minChildSize: 0.5,//작은
+                  maxChildSize: 0.99,//큰
+                  expand: false,
+                  builder:(context,scrollController){
+                return MyBottomSheet(scrollController:scrollController);
+              })
+                )]))
             ],
-          )
+          ),
       ),
-    );;
+    );
   }
 }

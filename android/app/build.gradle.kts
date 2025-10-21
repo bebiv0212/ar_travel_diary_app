5fc66832da4f9a41d52497b311869fbc6c330431
yoyowasi <youmin302@gmail.com>
2025-10-22 01:34:51 +0900

    Android Gradle 및 프로젝트 설정 파일 업데이트, 네임스페이스 및 권한 구성 정리

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,38 @@
+// android/app/build.gradle.kts
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.joljak"
+
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.joljak"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+        }
+        debug {
+            isDebuggable = true
         }
     }
-}
-
-flutter {
-    source = "../.."
 }
--- android/app/src/main/kotlin/com/example/joljak/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/joljak/MainActivity.kt
@@ -1,3 +1,4 @@
+// android/app/src/main/kotlin/com/example/joljak/MainActivity.kt
 package com.example.joljak
 
 import io.flutter.embedding.android.FlutterActivity
